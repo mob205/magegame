69cde0983271d41dda416946ca3b10fae1324f91
Donny M <Mobslayer205@gmail.com>
2020-09-13 19:05:41 -0400

    Added a shield spell that creates a barrier toward mouse a fixed distance away from player. Uses a better way of facing the 'projectile' toward cursor than fireball.

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -12,11 +12,15 @@ public class Fireball : Ability
     {
         if (!activeCooldown)
         {
-            // Gets direction from player to mouse pos. Creates and fires proj in direction.
+            // Gets direction from player to mouse pos.
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var direction = (mousePos - (Vector2)transform.position).normalized;
+
+            // Create and fire projectile
             var fireballInstance = Instantiate(projectile, transform.position, Quaternion.LookRotation(direction, Vector3.up));
             fireballInstance.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+
+            Destroy(fireballInstance, projectileLifetime);
 
             StartCooldown();
         }
---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : Ability
+{
+    [SerializeField] float shieldDuration;
+    [SerializeField] GameObject shieldObject;
+    [SerializeField] float followDist;
+
+    // Update is called once per frame
+    public override void CastAbility()
+    {
+        if (!activeCooldown)
+        {
+            // Gets direction from player to mouse pos.
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var direction = (mousePos - (Vector2)transform.position).normalized;
+
+            // Create shield object
+            var shieldInstance = Instantiate(shieldObject, transform.position, Quaternion.identity, transform);
+
+            // Face instance toward mouse
+            var pivotPos = transform.position;
+
+            mousePos.x -= pivotPos.x;
+            mousePos.y -= pivotPos.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            shieldInstance.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            // Add the following distance offset from player
+            shieldInstance.transform.position += shieldInstance.transform.forward * followDist;
+
+            Destroy(shieldInstance, shieldDuration);
+
+            StartCooldown();
+        }
+    }
+}
