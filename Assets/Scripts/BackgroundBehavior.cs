c23ea684d010001e49e4d38e668bc6de1ab03cde
Donny M <Mobslayer205@gmail.com>
2020-09-27 22:23:27 -0400

    Background now stays on the same Y level as camera and camera follows player within a set boundary.

---
+++ Assets/Scripts/BackgroundBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehavior : MonoBehaviour
+{
+    
+    float yOffset;
+    Camera cam;
+    void Start()
+    {
+        cam = Camera.main;
+        yOffset = transform.position.y - cam.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x, cam.transform.position.y + yOffset);
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] float smoothing = 0.1f;
+    [SerializeField] BoxCollider2D viewBox = null;
+
+    PlayerAbilities player;
+    Camera cam;
+    Vector2 minBounds, maxBounds;
+    void Start()
+    {
+        player = PlayerAbilities.instance;
+        cam = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        FollowPlayer();
+    }
+    void FollowPlayer()
+    {
+        
+        var min = viewBox.bounds.min;
+        var max = viewBox.bounds.max;
+        var x = player.transform.position.x;
+        var y = player.transform.position.y;
+
+        // Ortho size (Distance from center to top) multiplied by resolution for dist from center to side of screen
+        var cameraHalfX = cam.orthographicSize * ((float)Screen.width / Screen.height);
+
+        // Ensures camera stays within view box bounds
+        x = Mathf.Clamp(x, min.x + cameraHalfX, max.x - cameraHalfX);
+        y = Mathf.Clamp(y, min.y + cam.orthographicSize, max.y = cam.orthographicSize);
+
+        // Smoothly move camera to calculated position. Vector3 Lerp (vs 2) to preserve camera's z offset.
+        transform.position = Vector3.Lerp(transform.position, new Vector3(x, y, transform.position.z), smoothing);
+    }
+}
