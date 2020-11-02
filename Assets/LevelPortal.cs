dd68f400bf1c18973966a9b5a66d08ef4d267e57
Donny M <Mobslayer205@gmail.com>
2020-11-01 20:07:08 -0500

    Added level 3. Added portals that teleport player to another point of the level.

---
+++ Assets/LevelPortal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPortal : Interactable
+{
+    [SerializeField] Transform teleportLocation = null;
+
+    public override void Interact(Collider2D collision)
+    {
+        if(collision.gameObject == PlayerAbilities.instance.gameObject)
+        {
+            collision.gameObject.transform.position = teleportLocation.position;
+        }
+    }
+}
