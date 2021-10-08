a706374a1ec376b037aed896192cf854b0543521
Donny <zombieminer105@gmail.com>
2021-10-08 12:21:30 -0400

    Frozen enemies now play frozen particles

--- Assets/Scripts/Buffs/Buff Scripts/ImmobileBuff.cs
+++ Assets/Scripts/Buffs/Buff Scripts/ImmobileBuff.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ScriptableObjects;
 
 public class ImmobileBuff : TickBuff
 {
     private readonly Rigidbody2D _rb;
+
+    private ParticleSystem particles;
     public ImmobileBuff(ScriptableBuff buff, GameObject obj) : base(buff, obj)
     {
         _rb = obj.GetComponent<Rigidbody2D>();
     }
     protected override void ApplyEffect()
     {
+        ScriptableImmobileBuff immobileBuff = (ScriptableImmobileBuff)Buff;
+        particles = Object.Instantiate(immobileBuff.ImmobilizedParticles, Obj.transform);
     }
     public override void End()
     {
+        Object.Destroy(particles);
         EffectStacks = 0;
     }
     protected override void ApplyTickEffect()
--- Assets/Scripts/Buffs/ScriptableObjects/ScriptableImmobileBuff.cs
+++ Assets/Scripts/Buffs/ScriptableObjects/ScriptableImmobileBuff.cs
@@ -5,6 +5,7 @@ namespace ScriptableObjects
     [CreateAssetMenu(menuName = "Buffs/ImmobileBuff")]
     public class ScriptableImmobileBuff : ScriptableBuff
     {
+        public ParticleSystem ImmobilizedParticles;
         public override TimedBuff InitializeBuff(GameObject obj)
         {
             return new ImmobileBuff(this, obj);
