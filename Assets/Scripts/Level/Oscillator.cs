d7d4dfc2bc66ee6a22d3796f8b2f32e2b2970cf6
Donny <zombieminer105@gmail.com>
2021-10-16 11:28:39 -0400

    Removed On Fire spell cast time for more smooth combo-ing. Oscillator script now has an option to start oscillating when player first makes contact rather than when script is enabled. Started work on bonus level.

--- Assets/Scripts/Level/Oscillator.cs
+++ Assets/Scripts/Level/Oscillator.cs
@@ -6,13 +6,24 @@ public class Oscillator : MonoBehaviour
 {
     [SerializeField] Vector2 distance = new Vector2(0, 0);
     [SerializeField] float speed = 1;
+    [SerializeField] bool startOnContact;
+
     Vector2 startPos;
     float startTime;
+    bool hasStarted;
 
     //Dictionary<GameObject, Transform> originalParents = new Dictionary<GameObject, Transform>();
     private void OnEnable()
     {
-        if(startPos == new Vector2(0, 0))
+        if (!startOnContact)
+        {
+            StartMovement();
+        }
+    }
+    private void StartMovement()
+    {
+        hasStarted = true;
+        if (startPos == new Vector2(0, 0))
         {
             startPos = transform.position;
         }
@@ -22,16 +33,23 @@ public class Oscillator : MonoBehaviour
     private void FixedUpdate()
     {
         // Oscillate factor calculated through a sine function of time.
-        // 0.5f(x)+0.5 makes the range of f(x) 0 to 1 so it is usable by the Lerp function.
+        // f(x) = 0.5sin(x)+0.5 makes the range of f [0,1] so it is usable by the Lerp function.
         // Subtraction of pi / 2 from x recenters the wave so that f(0) = 0.
-        float oscillateFactor = 0.5f * (1 + Mathf.Sin(speed * (Time.time - startTime) - (Mathf.PI/2)));
-        transform.position = Vector2.Lerp(startPos, startPos + distance, oscillateFactor);
+        if (hasStarted)
+        {
+            float oscillateFactor = 0.5f * (1 + Mathf.Sin(speed * (Time.time - startTime) - (Mathf.PI / 2)));
+            transform.position = Vector2.Lerp(startPos, startPos + distance, oscillateFactor);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == PlayerAbilities.instance.gameObject)
         {
+            if (!hasStarted)
+            {
+                StartMovement();
+            }
             collision.gameObject.transform.parent = gameObject.transform;
             //originalParents.Add(collision.gameObject, collision.gameObject.transform.parent);
             //collision.gameObject.transform.parent = gameObject.transform;
