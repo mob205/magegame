ff228ac6f5d2aa2da79d08f34cc070d1ef196d99
Donny <zombieminer105@gmail.com>
2021-10-24 00:03:50 -0400

    Grassland(1-3) miniboss fight now uses boss room system. Portals can now trigger UnityEvents when being teleported

--- Assets/Scripts/Level/LevelPortal.cs
+++ Assets/Scripts/Level/LevelPortal.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LevelPortal : Interactable
 {
     [SerializeField] Transform teleportLocation = null;
+    [SerializeField] UnityEvent teleportEvent;
 
     public override void Interact(Collider2D collision)
     {
         if(collision.gameObject == PlayerAbilities.instance.gameObject)
         {
             collision.gameObject.transform.position = teleportLocation.position;
+            if (teleportEvent != null)
+            {
+                teleportEvent.Invoke();
+            }
         }
     }
 }
